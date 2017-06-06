3ed28bd8bf5b505eed990167ab4c4b20acd221d1
chriswebb09 <christopher.webb@flatironschool.com>
2017-06-06 11:11:46 -0400

    added tests

---
+++ PodCatcher/PodCatcherTests/PodcastsListTests.swift
@@ -0,0 +1,34 @@
+//
+//  PodcastsListTests.swift
+//  PodCatcher
+//
+//  Created by Christopher Webb-Orenstein on 6/6/17.
+//  Copyright © 2017 Christopher Webb-Orenstein. All rights reserved.
+//
+
+import XCTest
+@testable import PodCatcher
+
+class PodcastsListTests: XCTestCase {
+    
+    var podcastsListViewController: PodcastListViewController!
+    
+    override func setUp() {
+        super.setUp()
+        podcastsListViewController = PodcastListViewController()
+    }
+    
+    override func tearDown() {
+        podcastsListViewController = nil
+        super.tearDown()
+    }
+    
+    func testSetCaster() {
+        var caster = Caster()
+        caster.name = "Test Name"
+        podcastsListViewController.caster = caster
+        podcastsListViewController.viewDidLoad()
+        XCTAssertEqual(podcastsListViewController.title, "Test Name")
+    }
+    
+}
