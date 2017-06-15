e27ae5efbbf520b83c6c26636e2cc1520e8789a2
chriswebb09 <christopher.webb@flatironschool.com>
2017-06-15 11:14:28 -0400

    separated coordinator functionality into extension files

---
+++ PodCatcher/PodCatcher/Coordinators/Coordinator/MainCoordinator+Extension.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+extension MainCoordinator: CoordinatorDelegate {
+    
+    func transitionCoordinator(type: CoordinatorType, dataSource: BaseMediaControllerDataSource?) {
+        switch type {
+        case .app:
+            let firebaseAuth = Auth.auth()
+            do {
+                try firebaseAuth.signOut()
+            } catch let signOutError as NSError {
+                print ("Error signing out: %@", signOutError)
+            }
+            let newCoordinator = StartCoordinator(navigationController: UINavigationController(), window: window)
+            newCoordinator.delegate = self
+            newCoordinator.skipSplash()
+            self.appCoordinator = newCoordinator
+            self.appCoordinator.delegate = self
+            
+        case .tabbar:
+            let tabbarController = TabBarController()
+            self.dataSource = dataSource
+            tabbarController.dataSource = self.dataSource
+            
+            let tabbBarCoordinator = TabBarCoordinator(tabBarController: tabbarController, window: window)
+            guard let dataSource = dataSource else { return }
+            
+            let mediaViewController = MediaCollectionViewController(dataSource: dataSource)
+            let mediaTab = UINavigationController(rootViewController: mediaViewController)
+            tabbBarCoordinator.setupMediaCoordinator(navigationController: mediaTab, dataSource: dataSource)
+            let mediaCoord = tabbBarCoordinator.childCoordinators[0] as! MediaTabCoordinator
+            mediaCoord.delegate = self
+            
+            let model = SettingsViewModel(firstSettingOptionText: "OptionOne", secondSettingOptionText: "OptionTwo")
+            let settingsView = SettingsView(frame: CGRect.zero, model: model)
+            let settingsViewController = SettingsViewController(settingsView: settingsView, dataSource: dataSource)
+            let settingsTab = UINavigationController(rootViewController: settingsViewController)
+            
+            tabbBarCoordinator.setupSettingsCoordinator(navigationController: settingsTab, dataSource: dataSource)
+            tabbBarCoordinator.delegate = self
+            let settingsCoord = tabbBarCoordinator.childCoordinators[1] as! SettingsTabCoordinator
+            settingsCoord.delegate = self
+            
+            appCoordinator = tabbBarCoordinator
+            start()
+        }
+    }
+}
--- PodCatcher/PodCatcher/Coordinators/Tab/SettingsTabCoordinator.swift
+++ PodCatcher/PodCatcher/Coordinators/Tab/SettingsTabCoordinator.swift
@@ -25,33 +25,3 @@ class SettingsTabCoordinator: NavigationCoordinator {
     }
 }
 
-extension SettingsTabCoordinator: SettingsViewControllerDelegate {
-    func guestUserSignInTapped(tapped: Bool) {
-        delegate?.transitionCoordinator(type: .app, dataSource: dataSource)
-    }
-    
-    func settingTwoTapped(tapped: Bool) {
-        let updateAccountViewController = UpdateAccountViewController()
-        updateAccountViewController.dataSource = dataSource
-        updateAccountViewController.delegate = self
-        navigationController.viewControllers.append(updateAccountViewController)
-    }
-
-    func settingOneTapped(tapped: Bool) {
-        let updateAccountViewController = UpdateAccountViewController()
-        updateAccountViewController.dataSource = dataSource
-        updateAccountViewController.delegate = self
-        navigationController.viewControllers.append(updateAccountViewController)
-    }    
-}
-
-extension SettingsTabCoordinator: UpdateAccountViewControllerDelegate {
-    func updated(username: String) {
-        print(username)
-    }
-    
-    func updated(email: String) {
-        
-    }
-
-}
