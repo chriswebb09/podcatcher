38f771f98aad0676c04102d90dd2eda60ca23b75
chriswebb09 <christopher.webb@flatironschool.com>
2017-07-04 11:09:23 -0400

    update

---
+++ PodCatcher/PodCatcher/ApplicationServices/Data/BaseDataStore.swift
@@ -0,0 +1,58 @@
+import UIKit
+
+class BaseDataStore: NSObject {
+    
+    var searchTerm: String = ""
+    
+    func setSearch(term: String) {
+        searchTerm = term
+    }
+    
+    var lookup: String = ""
+    
+    func searchForTracksFromLookup(completion: @escaping (_ results: [CasterSearchResult]? , _ error: Error?) -> Void) {
+        iTunesAPIClient.search(forLookup: lookup) { response in
+            switch response {
+            case .success(let data):
+                let resultsData = data["results"] as! [[String: Any]]
+                let results = ResultsParser.parse(resultsData: resultsData)
+                completion(results, nil)
+            case .failed(let error):
+                completion(nil, error)
+            }
+        }
+    }
+    
+    func searchForTracks(completion: @escaping (_ results: [CasterSearchResult]? , _ error: Error?) -> Void) {
+        iTunesAPIClient.search(for: searchTerm) { response in
+            switch response {
+            case .success(let data):
+                let resultsData = data["results"] as! [[String: Any]]
+                let results = ResultsParser.parse(resultsData: resultsData)
+                completion(results, nil)
+            case .failed(let error):
+                completion(nil, error)
+            }
+        }
+    }
+    
+    static func userSignIn(username: String, password: String, completion: @escaping (PodCatcherUser?, Error?) -> Void) {
+        UserDataAPIClient.loginToAccount(email: username, password: password) { user, error in
+            print("here")
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil, error)
+                return
+            }
+            PullData.pullFromDatabase { pulled in
+                dump(pulled)
+                guard let user = user else { return }
+                pulled.username = user.uid
+                pulled.userId = user.uid
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    completion(pulled, nil)
+                }
+            }
+        }
+    }
+}
--- PodCatcher/PodCatcher/ApplicationServices/Data/PodCatcherUserDataStore.swift
+++ PodCatcher/PodCatcher/ApplicationServices/Data/PodCatcherUserDataStore.swift
@@ -1,24 +1,8 @@
 import Foundation
 
-class PodCatcherUserDataStore {
+class PodCatcherUserDataStore: BaseDataStore {
     
-    static func userSignIn(username: String, password: String, completion: @escaping (PodCatcherUser?, Error?) -> Void) {
-        UserDataAPIClient.loginToAccount(email: username, password: password) { user, error in
-            print("here")
-            if let error = error {
-                print(error.localizedDescription)
-                completion(nil, error)
-                return
-            }
-            PullData.pullFromDatabase { pulled in
-                dump(pulled)
-                guard let user = user else { return }
-                pulled.username = user.uid
-                pulled.userId = user.uid
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                    completion(pulled, nil)
-                }
-            }
-        }
-    }
+    let store = TrackDataStore()
+    var items = [PodcastSearchResult]()
+
 }
