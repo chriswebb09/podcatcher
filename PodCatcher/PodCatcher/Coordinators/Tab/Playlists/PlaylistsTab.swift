ad59d225576cb03a30ea70afb19f0ebae58a02b3
chriswebb09 <christopher.webb@flatironschool.com>
2017-07-04 18:57:18 -0400

    update

---
+++ PodCatcher/PodCatcher/Coordinators/Tab/Playlists/PlaylistsTab.swift
@@ -0,0 +1,9 @@
+//
+//  PlaylistsTab.swift
+//  PodCatcher
+//
+//  Created by Christopher Webb-Orenstein on 7/4/17.
+//  Copyright © 2017 Christopher Webb-Orenstein. All rights reserved.
+//
+
+import Foundation
---
+++ PodCatcher/PodCatcher/UI/Screens/Playlists/PlaylistCell.swift
@@ -0,0 +1,9 @@
+//
+//  PlaylistCell.swift
+//  PodCatcher
+//
+//  Created by Christopher Webb-Orenstein on 7/4/17.
+//  Copyright © 2017 Christopher Webb-Orenstein. All rights reserved.
+//
+
+import Foundation
