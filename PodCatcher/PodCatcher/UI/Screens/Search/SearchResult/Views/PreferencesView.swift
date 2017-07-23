ab2ab4c61ec707ff2ec75b65a64dbdcbcab99bfe
chriswebb09 <christopher.webb@flatironschool.com>
2017-07-23 19:49:50 -0400

    fixed bug

---
+++ PodCatcher/PodCatcher/UI/Screens/Search/SearchResult/Views/PreferencesView.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+final class PreferencesView: UIView {
+    
+    weak var delegate: PreferencesViewDelegate?
+    
+    // MARK: - UI Properties
+    
+    var moreMenuButton: UIButton = {
+        var moreMenuButton = UIButton()
+        moreMenuButton.setImage(#imageLiteral(resourceName: "more-button-white"), for: .normal)
+        return moreMenuButton
+    }()
+    
+    // MARK: - Configuration Methods
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setupConstraints()
+        moreMenuButton.addTarget(self, action: #selector(moreButton(tapped:)), for: .touchUpInside)
+        backgroundColor = .lightGray
+    }
+    
+    func setupConstraints() {
+        setup(moreButton: moreMenuButton)
+    }
+    
+    func setup(moreButton: UIButton) {
+        addSubview(moreButton)
+        moreButton.translatesAutoresizingMaskIntoConstraints = false
+        moreButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        moreButton.heightAnchor.constraint(equalTo: heightAnchor, multiplier: PreferencesViewConstants.tagButtonHeightMultiplier).isActive = true
+        moreButton.rightAnchor.constraint(equalTo: rightAnchor, constant: PreferencesViewConstants.moreButtonRightOffset).isActive = true
+    }
+    
+    func moreButton(tapped: Bool) {
+        delegate?.moreButton(tapped: tapped)
+    }
+    
+    func addTagButton(tapped: Bool) {
+        delegate?.addTagButton(tapped: tapped)
+    }
+}
---
+++ PodCatcher/PodCatcher/UI/Screens/Search/SearchResult/Views/PreferencesViewDelegate.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+protocol PreferencesViewDelegate: class {
+    func moreButton(tapped: Bool)
+    func addTagButton(tapped: Bool)
+}
