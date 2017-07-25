29f4baed1b94969a715a8b09a54ebb87f0d04ff1
chriswebb09 <christopher.webb@flatironschool.com>
2017-07-25 18:58:14 -0400

    fixed bug

--- PodCatcher/PodCatcher/UI/Constants/PodcastCellConstants.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-struct PodcastCellConstants {
-    static let podcastTitleLabelWidthMultiplier: CGFloat = 0.76
-    static let podcastTitleLabelLeftOffset: CGFloat = UIScreen.main.bounds.width * 0.04
-    static let playtimeLabelRightOffset: CGFloat = UIScreen.main.bounds.width * -0.045
-    static let playtimeLabelWidthMultiplier: CGFloat = 0.2
-}
--- PodCatcher/PodCatcher/UI/Constants/PodcastListConstants.swift
+++ PodCatcher/PodCatcher/UI/Constants/PodcastListConstants.swift
@@ -13,3 +13,29 @@ struct PodcastListConstants {
     static let emptyCastViewFrame = CGRect(x: PodcastListConstants.topFrame.minX, y: PodcastListConstants.topFrame.maxY, width: PodcastListConstants.topFrameWidth, height: PodcastListConstants.topFrameHeight + 10)
 }
 
+struct PodcastCellConstants {
+    static let podcastTitleLabelWidthMultiplier: CGFloat = 0.76
+    static let podcastTitleLabelLeftOffset: CGFloat = UIScreen.main.bounds.width * 0.04
+    static let playtimeLabelRightOffset: CGFloat = UIScreen.main.bounds.width * -0.045
+    static let playtimeLabelWidthMultiplier: CGFloat = 0.2
+}
+
+struct PodcastListTopViewConstants {
+    static let podcastImageViewCenterYOffset: CGFloat = UIScreen.main.bounds.height * -0.025
+    static let preferencesViewHeightMultiplier: CGFloat = 0.12
+    static let tagsViewHeightMultiplier: CGFloat = 0.13
+    static let podcastImageViewHeightMultiplier: CGFloat = 0.8
+    static let podcastImageViewWidthMultiplier: CGFloat = 0.76
+    static let titleLabelHeightMultiplier: CGFloat = 0.3
+    static let titleLabelTopOffset: CGFloat = UIScreen.main.bounds.height * 0.0008
+}
+
+struct BrowseListTopViewConstants {
+    static let podcastImageViewCenterYOffset: CGFloat = UIScreen.main.bounds.height * -0.01
+    static let preferencesViewHeightMultiplier: CGFloat = 0.12
+    static let tagsViewHeightMultiplier: CGFloat = 0.13
+    static let podcastImageViewHeightMultiplier: CGFloat = 0.72
+    static let podcastImageViewWidthMultiplier: CGFloat = 0.82
+    static let titleLabelHeightMultiplier: CGFloat = 0.3
+    static let titleLabelTopOffset: CGFloat = UIScreen.main.bounds.height * 0.0008
+}
