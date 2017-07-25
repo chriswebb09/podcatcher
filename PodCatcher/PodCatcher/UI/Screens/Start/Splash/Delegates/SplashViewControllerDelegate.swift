f86ebf9d11087d73461caaf9fb7d943b3f5c80ad
chriswebb09 <christopher.webb@flatironschool.com>
2017-07-25 19:24:27 -0400

    fixed bug

--- PodCatcher/PodCatcher/UI/Screens/Start/Splash/Delegates/SplashViewControllerDelegate.swift
+++ PodCatcher/PodCatcher/UI/Screens/Start/Splash/Delegates/SplashViewControllerDelegate.swift
@@ -3,3 +3,7 @@ import Foundation
 protocol SplashViewControllerDelegate: class {
     func splashAnimation(finished: Bool)
 }
+
+protocol SplashViewDelegate: class {
+    func animation(_ isComplete: Bool)
+}
--- PodCatcher/PodCatcher/UI/Screens/Start/Splash/Delegates/SplashViewDelegate.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-protocol SplashViewDelegate: class {
-    func animation(_ isComplete: Bool)
-}
