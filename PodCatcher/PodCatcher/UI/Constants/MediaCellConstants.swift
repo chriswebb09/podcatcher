d6a4fa21b896584a5d82236b86cf1172c9c066f0
chriswebb09 <christopher.webb@flatironschool.com>
2017-07-25 18:44:06 -0400

    fixed bug

--- PodCatcher/PodCatcher/UI/Constants/MediaCellConstants.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-struct MediaCellConstants {
-    static let smallFont = UIFont(name: "AvenirNext-Regular", size: 10)
-    static let albumHeightMultiplier: CGFloat =  0.75
-    static let labelHeightMultiplier: CGFloat = 0
-}
--- PodCatcher/PodCatcher/UI/Constants/PodcastListTopViewConstants.swift
+++ PodCatcher/PodCatcher/UI/Constants/PodcastListTopViewConstants.swift
@@ -9,3 +9,13 @@ struct PodcastListTopViewConstants {
     static let titleLabelHeightMultiplier: CGFloat = 0.3
     static let titleLabelTopOffset: CGFloat = UIScreen.main.bounds.height * 0.0008
 }
+
+struct BrowseListTopViewConstants {
+    static let podcastImageViewCenterYOffset: CGFloat = UIScreen.main.bounds.height * -0.01
+    static let preferencesViewHeightMultiplier: CGFloat = 0.12
+    static let tagsViewHeightMultiplier: CGFloat = 0.13
+    static let podcastImageViewHeightMultiplier: CGFloat = 0.72
+    static let podcastImageViewWidthMultiplier: CGFloat = 0.82
+    static let titleLabelHeightMultiplier: CGFloat = 0.3
+    static let titleLabelTopOffset: CGFloat = UIScreen.main.bounds.height * 0.0008
+}
