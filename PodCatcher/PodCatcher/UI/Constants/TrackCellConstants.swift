a52cabb021164592222e4209b8aaedc6751f6421
chriswebb09 <christopher.webb@flatironschool.com>
2017-07-25 18:28:09 -0400

    fixed bug

--- PodCatcher/PodCatcher/UI/Constants/TrackCellConstants.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-struct TrackCellConstants {
-    static let smallFont = UIFont(name: "AvenirNext-Regular", size: 10)
-    static let albumHeightMultiplier: CGFloat =  0.86
-    static let labelHeightMultiplier: CGFloat = 0.2
-}
