2a05b4cf4abce91f4e6003324f59103b06ea1352
chriswebb09 <christopher.webb@flatironschool.com>
2017-07-25 19:10:12 -0400

    fixed bug

--- PodCatcher/PodCatcher/ApplicationServices/NetworkServices/iTunesAPIClient.swift
+++ PodCatcher/PodCatcher/ApplicationServices/NetworkServices/iTunesAPIClient.swift
@@ -1,5 +1,44 @@
 import UIKit
 
+import Foundation
+
+typealias JSON = [String : Any]
+
+enum Response {
+    case success(JSON?), failed(Error)
+}
+
+struct URLConstructor {
+    
+    var searchTerm: String
+    
+    func build(searchTerm: String) -> URL? {
+        guard let encodedQuery = searchTerm.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else { return nil }
+        let urlString = URLRouter.base.url + URLRouter.path.url + encodedQuery
+        print(urlString)
+        return URL(string: urlString)
+    }
+}
+
+enum URLRouter {
+    
+    case base, path
+    
+    var url: String {
+        switch self {
+        case .base:
+            return "https://itunes.apple.com"
+        case .path:
+            return "/search?country=US&media=podcast&limit=15&term="
+            
+        }
+    }
+}
+
+enum URLPath {
+    case podcastAuth(searchTerm: String), keyTerm(searchTerm: String)
+}
+
 class RSSFeedAPIClient: NSObject {
     
     static func requestFeed(for urlString: String, completion: @escaping ([[String: String]]?, Error?) -> Void) {
