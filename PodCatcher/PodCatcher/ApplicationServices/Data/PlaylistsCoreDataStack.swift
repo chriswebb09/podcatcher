81804df50a742d05c4332198eae200162b812755
chriswebb09 <christopher.webb@flatironschool.com>
2017-07-26 16:43:01 -0400

    fixed bug

---
+++ PodCatcher/PodCatcher/ApplicationServices/Data/PlaylistsCoreDataStack.swift
@@ -0,0 +1,40 @@
+import UIKit
+import CoreData
+
+class PlaylistsCoreDataStack {
+    
+    var playlists: [NSManagedObject] = []
+    
+    func save(name: String, uid: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.coreData.managedContext
+        guard let entity = NSEntityDescription.entity(forEntityName: "PodcastPlaylist", in: managedContext) else { return }
+        let playlist = NSManagedObject(entity: entity, insertInto: managedContext)
+        let id = UUID().uuidString
+        let date = NSDate()
+        playlist.setValue(name, forKeyPath: "playlistName")
+        playlist.setValue(id, forKey: "playlistId")
+        playlist.setValue(date, forKey: "dateCreated")
+        playlist.setValue(0, forKey: "timeSpentListening")
+        playlist.setValue(0, forKey: "numberOfItems")
+        playlist.setValue(0, forKey: "numberOfPlays")
+        playlist.setValue(uid, forKey: "uid")
+        do {
+            try managedContext.save()
+            playlists.append(playlist)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func fetchFromCore() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.coreData.managedContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "PodcastPlaylist")
+        do {
+            playlists = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+}
