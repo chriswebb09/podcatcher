306bbe38d9dedd8a8298c2266bd9e6e6d6410b2d
chriswebb09 <christopher.webb@flatironschool.com>
2017-07-26 01:00:23 -0400

    fixed bug

--- PodCatcher/PodCatcher/Coordinators/Tab/Tab/TabControllerCoordinator.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-protocol TabControllerCoordinator: Coordinator {
-    var window: UIWindow! { get set }
-    var tabBarController: TabBarController { get set }
-    var childCoordinators: [NavigationCoordinator] { get set }
-}
--- PodCatcher/PodCatcher/Coordinators/Tab/Tab/TabCoordinator.swift
+++ PodCatcher/PodCatcher/Coordinators/Tab/Tab/TabCoordinator.swift
@@ -1,5 +1,11 @@
 import UIKit
 
+protocol TabControllerCoordinator: Coordinator {
+    var window: UIWindow! { get set }
+    var tabBarController: TabBarController { get set }
+    var childCoordinators: [NavigationCoordinator] { get set }
+}
+
 final class TabCoordinator: NavigationCoordinator {
     
     var type: CoordinatorType = .tabbar
