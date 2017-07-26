6c585da1df92f2f3bca5b5f4f58692fdf0af5051
chriswebb09 <christopher.webb@flatironschool.com>
2017-07-26 16:31:49 -0400

    fixed bug

--- PodCatcher/PodCatcher/UI/Elements/Popover/Menu/BaseMenuPopoverAlert.swift
+++ PodCatcher/PodCatcher/UI/Elements/Popover/Menu/BaseMenuPopoverAlert.swift
@@ -15,38 +15,3 @@ class BaseMenuPopoverAlert: UIView {
         viewController.view.sendSubview(toBack: containerView)
     }
 }
-
-class BottomMenu {
-    
-    var menu: MenuView = {
-        let menuView = MenuView()
-        menuView.isUserInteractionEnabled = true
-        return menuView
-    }()
-    
-    func setMenu(_ size: CGSize) {
-        menu.frame.size = size
-    }
-    
-    func setMenu(_ origin: CGPoint) {
-        menu.frame.origin = origin
-    }
-    
-    func setMenu(color: UIColor, borderColor: UIColor, textColor: UIColor) {
-        menu.setMenuColor(backgroundColor: color, borderColor: borderColor, labelTextColor: textColor)
-    }
-    
-    func setupMenu() {
-        menu.isUserInteractionEnabled = true
-        menu.configureView()
-    }
-    
-    func showOn(_ view: UIView) {
-        view.addSubview(menu)
-        view.bringSubview(toFront: menu)
-    }
-    
-    func hideFrom(_ view: UIView) {
-        view.sendSubview(toBack: menu)
-    }
-}
