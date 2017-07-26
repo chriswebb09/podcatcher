4d6f8444b61d0aef19187958d14def018a241cbc
chriswebb09 <christopher.webb@flatironschool.com>
2017-07-26 02:53:18 -0400

    fixed bug

--- PodCatcher/PodCatcher/UI/Screens/Player/Controllers/PlayerViewController.swift
+++ PodCatcher/PodCatcher/UI/Screens/Player/Controllers/PlayerViewController.swift
@@ -3,6 +3,25 @@ import CoreMedia
 import CoreData
 import AVFoundation
 
+protocol PlayerViewControllerDelegate: class {
+    func playButton(tapped: Bool)
+    func pauseButton(tapped: Bool)
+    func skipButton(tapped: Bool)
+    func navigateBack(tapped: Bool)
+    func addItemToPlaylist(item: CasterSearchResult, index: Int)
+}
+
+protocol PlayerViewDelegate: class {
+    func playButtonTapped()
+    func pauseButtonTapped()
+    func skipButtonTapped()
+    func backButtonTapped()
+    func moreButton(tapped: Bool)
+    func updateTimeValue(time: Double)
+    func navigateBack(tapped: Bool)
+    //   func loading()
+}
+
 final class PlayerViewController: BaseViewController {
     
     weak var delegate: PlayerViewControllerDelegate?
--- PodCatcher/PodCatcher/UI/Screens/Player/Delegates/PlayerViewControllerDelegate.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-protocol PlayerViewControllerDelegate: class {
-    func playButton(tapped: Bool)
-    func pauseButton(tapped: Bool)
-    func skipButton(tapped: Bool)
-    func navigateBack(tapped: Bool)
-    func addItemToPlaylist(item: CasterSearchResult, index: Int)
-}
