4c0d375608ffd15f7b59705f409f135d9eced053
chriswebb09 <christopher.webb@flatironschool.com>
2017-07-26 16:44:18 -0400

    fixed bug

---
+++ PodCatcher/PodCatcher/ApplicationServices/Data/CoreDataStack.swift
@@ -0,0 +1,34 @@
+import UIKit
+import CoreData
+
+class CoreDataStack {
+    
+    private let modelName: String
+    
+    private lazy var storeContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: self.modelName)
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error as NSError? {
+                print("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    lazy var managedContext: NSManagedObjectContext = {
+        return self.storeContainer.viewContext
+    }()
+    
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+    
+    func saveContext () {
+        guard managedContext.hasChanges else { return }
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Unresolved error \(error), \(error.userInfo)")
+        }
+    }
+}
