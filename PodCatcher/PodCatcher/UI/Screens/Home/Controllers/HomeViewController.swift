af13739e9d595b39ed153ab6d809b32b6038478d
chriswebb09 <christopher.webb@flatironschool.com>
2017-07-26 02:48:23 -0400

    fixed bug

--- PodCatcher/PodCatcher/UI/Screens/Home/Controllers/HomeViewController.swift
+++ PodCatcher/PodCatcher/UI/Screens/Home/Controllers/HomeViewController.swift
@@ -1,6 +1,12 @@
 import UIKit
 import CoreData
 
+protocol HomeViewControllerDelegate: class {
+    func didSelect(at index: Int, with caster: PodcastSearchResult, image: UIImage)
+    func didSelect(at index: Int, with subscription: Subscription, image: UIImage)
+    func logout(tapped: Bool)
+}
+
 enum HomeInteractionMode {
     case subscription, edit
 }
--- PodCatcher/PodCatcher/UI/Screens/Home/Delegates/HomeViewControllerDelegate.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-protocol HomeViewControllerDelegate: class {
-    func didSelect(at index: Int, with caster: PodcastSearchResult, image: UIImage)
-    func didSelect(at index: Int, with subscription: Subscription, image: UIImage)
-    func logout(tapped: Bool)
-}
