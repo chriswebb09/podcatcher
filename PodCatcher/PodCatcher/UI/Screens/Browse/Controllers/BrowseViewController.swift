6634388f1788e2205c8ba4d36f2f80d94caeb0bf
chriswebb09 <christopher.webb@flatironschool.com>
2017-07-26 02:14:37 -0400

    fixed bug

--- PodCatcher/PodCatcher/UI/Screens/Browse/Controllers/BrowseViewController.swift
+++ PodCatcher/PodCatcher/UI/Screens/Browse/Controllers/BrowseViewController.swift
@@ -7,6 +7,16 @@ protocol BrowseViewControllerDelegate: class {
     func logout(tapped: Bool)
 }
 
+final class BrowseItemsFlowLayout: UICollectionViewFlowLayout {
+    
+    func setup() {
+        scrollDirection = .horizontal
+        itemSize = CGSize(width: UIScreen.main.bounds.width / 2, height: UIScreen.main.bounds.height / 2.8)
+        sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 15)
+        minimumLineSpacing = 30
+    }
+}
+
 final class BrowseViewController: BaseCollectionViewController {
     
     weak var delegate: BrowseViewControllerDelegate?
