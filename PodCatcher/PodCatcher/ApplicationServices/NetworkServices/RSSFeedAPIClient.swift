1b105ff8787186a539b7dd571fbaafa9c137a628
chriswebb09 <christopher.webb@flatironschool.com>
2017-07-27 00:39:19 -0400

    fixed player bug

--- PodCatcher/PodCatcher/ApplicationServices/NetworkServices/RSSFeedAPIClient.swift
+++ PodCatcher/PodCatcher/ApplicationServices/NetworkServices/RSSFeedAPIClient.swift
@@ -2,29 +2,34 @@ import UIKit
 
 class RSSFeedAPIClient: NSObject {
     
+    static func process(urlString: String, data: Data, completion: @escaping ([[String: String]]?) -> Void) {
+        let nprParse = ["344098539", "510298", "510200", "510318", "510208", "510282", "500005", "510307", "510322", "510308", "510310", "510019", "510313", "510289", "381444908", "510299"]
+        for npr in nprParse {
+            if urlString.hasSuffix(npr) {
+                let rssParser = NPRParser()
+                rssParser.recordKey = "item"
+                rssParser.dictionaryKeys = ["itunes:new-feed-url", "itunes:summary", " itunes:author", "tunes:subtitle", "pubDate", "enclosure", "itunes:duration", "title", "audio/mp3", "audio/mpeg", "itunes:keywords", "itunes:image", "category", "itunes:author", "itunes:summary", "description", "enclosure"]
+                rssParser.parseResponse(data) { parsedRSS in
+                    DispatchQueue.main.async {
+                        completion(parsedRSS)
+                    }
+                }
+            }
+        }
+    }
+    
     static func requestFeed(for urlString: String, completion: @escaping ([[String: String]]?, Error?) -> Void) {
         guard let url = URL(string: urlString) else { return }
-        let nprParse = ["344098539", "510298", "510200", "510318", "510208", "510282", "500005", "510307", "510322", "510308", "510310", "510019", "510313", "510289", "381444908", "510299"]
         URLSession(configuration: .ephemeral).dataTask(with: URLRequest(url: url)) { data, response, error in
             if let error = error {
-                print(error.localizedDescription)
                 DispatchQueue.main.async {
                     completion(nil, error)
                 }
             } else {
                 guard let data = data else { return }
                 if urlString.hasPrefix("https://www.npr.org/rss/podcast.php") {
-                    for npr in nprParse {
-                        if urlString.hasSuffix(npr) {
-                            let rssParser = NPRParser()
-                            rssParser.recordKey = "item"
-                            rssParser.dictionaryKeys = ["itunes:new-feed-url", "itunes:summary", " itunes:author", "tunes:subtitle", "pubDate", "enclosure", "itunes:duration", "title", "audio/mp3", "audio/mpeg", "itunes:keywords", "itunes:image", "category", "itunes:author", "itunes:summary", "description", "enclosure"]
-                            rssParser.parseResponse(data) { parsedRSS in
-                                DispatchQueue.main.async {
-                                    completion(parsedRSS, nil)
-                                }
-                            }
-                        }
+                    RSSFeedAPIClient.process(urlString: urlString, data: data) { processedResponse in
+                        completion(processedResponse, nil)
                     }
                 } else {
                     let rssParser = RSSParser()
@@ -43,7 +48,6 @@ extension RSSFeedAPIClient: XMLParserDelegate {
         guard let url = URL(string: "https://rss.itunes.apple.com/api/v1/us/podcasts/top-podcasts/40/explicit/xml") else { return }
         URLSession(configuration: .ephemeral).dataTask(with: URLRequest(url: url)) { data, response, error in
             if let error = error {
-                print(error.localizedDescription)
                 DispatchQueue.main.async {
                     completion(nil, error)
                 }
