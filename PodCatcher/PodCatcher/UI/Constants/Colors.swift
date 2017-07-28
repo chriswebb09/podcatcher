9e9ba7aa43796d1f000f29817b54e64c31c6a1f9
chriswebb09 <christopher.webb@flatironschool.com>
2017-07-27 20:39:11 -0400

    fixed bug

--- PodCatcher/PodCatcher/UI/Constants/Colors.swift
+++ PodCatcher/PodCatcher/UI/Constants/Colors.swift
@@ -45,12 +45,3 @@ struct MenuOptionViewConstants {
     static let iconViewHeightAnchor: CGFloat = 0.35
     static let iconViewWidthAnchor: CGFloat = 0.5
 }
-
-struct UnderlineTextFieldConstants {
-    static let newBoundsOriginXOffset: CGFloat = 22
-    static let newBoundsOriginYOffset: CGFloat = 8
-    static let newBoundsWidthOffset: CGFloat = 12
-    static let newBoundsTextRectXOffset: CGFloat = 5
-    static let forBoundsTextRectOriginXOffset: CGFloat = 50
-    static let forBoundsTectRectOriginYOffset: CGFloat = 12
-}
--- PodCatcher/PodCatcher/UI/Elements/Popover/Entry/EntryView.swift
+++ PodCatcher/PodCatcher/UI/Elements/Popover/Entry/EntryView.swift
@@ -1,5 +1,24 @@
 import UIKit
 
+class TextFieldExtension: UITextField {
+    
+    // Sets textfield input to + 10 inset on origin x value
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRect(x: bounds.origin.x + 12,
+                      y: bounds.origin.y,
+                      width: bounds.width + 12,
+                      height: bounds.height)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRect(x: bounds.origin.x + 12,
+                      y: bounds.origin.y,
+                      width: bounds.width + 12,
+                      height: bounds.height)
+    }
+}
+
 final class EntryView: UIView {
     
     // Input for playlist name
