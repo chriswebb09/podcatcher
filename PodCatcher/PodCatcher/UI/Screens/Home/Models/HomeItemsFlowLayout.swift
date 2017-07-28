d6693c19ae1baba43780d4be63dec6dc5474598d
chriswebb09 <christopher.webb@flatironschool.com>
2017-07-28 04:10:51 -0400

    fixed bug

---
+++ PodCatcher/PodCatcher/UI/Screens/Home/Models/HomeItemsFlowLayout.swift
@@ -0,0 +1,9 @@
+//
+//  HomeItemsFlowLayout.swift
+//  PodCatcher
+//
+//  Created by Christopher Webb-Orenstein on 7/27/17.
+//  Copyright © 2017 Christopher Webb-Orenstein. All rights reserved.
+//
+
+import Foundation
